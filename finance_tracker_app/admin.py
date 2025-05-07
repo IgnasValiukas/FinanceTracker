f389b58e24751f1bccf91b0f5bc1a22e1817fc9b
Ignas Val <ignas.valiukas@gmail.com>
2025-05-07 16:44:22 +0300

    Updated Models classes, deleted Account class

--- finance_tracker_app/admin.py
+++ finance_tracker_app/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Category, Transaction, Profile, Account
+from .models import Category, Transaction, Profile
 
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'type', 'title', 'category', 'date', 'description', 'profile')
+
+
+admin.site.register(Profile)
 admin.site.register(Category)
 admin.site.register(Transaction)
-admin.site.register(Profile)
-admin.site.register(Account)
+
--- finance_tracker_app/models.py
+++ finance_tracker_app/models.py
@@ -2,19 +2,28 @@ from django.db import models
 from django.db.models import TextField
 from django.contrib.auth.models import User
 
+TYPE_CHOICES = [
+    ('i', 'Income'),
+    ('e', 'Expense')
+]
+
 
 class Category(models.Model):
-    name = models.CharField('Category Name', max_length=200, help_text='Add category', unique=True)
+    name = models.CharField('Category Name', max_length=200, help_text='Add category')
+    type = models.CharField(max_length=1, choices=TYPE_CHOICES, default='e')
 
     def __str__(self):
-        return self.name
+        return f'{self.name} - {self.get_type_display()}'
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        constraints = [
+            models.UniqueConstraint(fields=['name', 'type'], name='unique_category_type')
+        ]
 
 
 class Transaction(models.Model):
-    TYPE_CHOICES = [
-        ('i', 'Income'),
-        ('e', 'Expense')
-    ]
     amount = models.DecimalField('Amount of money', help_text='Add amount of money', blank=False, null=False,
                                  default=0.0, max_digits=12, decimal_places=2)
     type = models.CharField(max_length=1, choices=TYPE_CHOICES, default='e')
@@ -22,10 +31,15 @@ class Transaction(models.Model):
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=False)
     date = models.DateField(help_text='Add transaction date')
     description = TextField(max_length=2000, blank=True, help_text='Write description (optional)')
-    account = models.ForeignKey('Account', on_delete=models.CASCADE, null=True)
+    client = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.account.profile.user}: {self.amount}, {self.type}, {self.title}, {self.category}, {self.date}'
+        return f'{self.client}: {self.amount}€ - {self.category.name}, {self.get_type_display()} ({self.date})'
+
+    class Meta:
+        verbose_name = 'Transaction'
+        verbose_name_plural = 'Transactions'
 
 
 class Profile(models.Model):
@@ -33,15 +47,10 @@ class Profile(models.Model):
     image = models.ImageField(default="profile_pics/default_profile.png")
 
     def __str__(self):
-        return f'{self.user}'
+        return f'{self.user.username}'
+
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
 
     # later add logic
-
-
-class Account(models.Model):
-    profile = models.OneToOneField('Profile', on_delete=models.SET_NULL, null=True, blank=False)
-    balance = models.DecimalField('Account balance', blank=False, null=False, default=0.0, max_digits=12,
-                                  decimal_places=2)
-
-    def __str__(self):
-        return f'{self.profile.user}'
